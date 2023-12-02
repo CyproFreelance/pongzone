1d262224a02787344a20066cf30015ffb370bbfa
Precise Goals <elitesarthakamr@gmail.com>
2023-12-02 21:22:37 +0530

    succeful working databse has been setted up

--- src/containers/home.jsx
+++ src/containers/home.jsx
@@ -1,23 +1,47 @@
+// eslint-disable-next-line
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import { GoogleLogin, GoogleOAuthProvider } from '@react-oauth/google';
-import { jwtDecode } from "jwt-decode";
+import { jwtDecode } from 'jwt-decode';
 import logo from '../assets/logo.png';
+import { storeUserData } from '../firebaseservice';
 
-const client_id = "166750811137-et1b6hjpveudcbk6n5p3913dpfu7fd7e.apps.googleusercontent.com";
+const client_id = '166750811137-et1b6hjpveudcbk6n5p3913dpfu7fd7e.apps.googleusercontent.com';
 
 export const Home = () => {
   const navigate = useNavigate();
+
+  const responseGoogle = (decoded) => {
+    const { profileObj } = decoded;
+
+    if (profileObj) {
+      const email = profileObj.email;
+      const displayName = profileObj.name;
+
+      localStorage.setItem('userEmail', email);
+      localStorage.setItem('displayName', displayName);
+      storeUserData(email, displayName);
+      navigate('/matchmaking');
+    } else {
+      const email = decoded.email;
+      const displayName = decoded.name;
+
+      localStorage.setItem('userEmail', email);
+      localStorage.setItem('displayName', displayName);
+      storeUserData(email, displayName);
+      navigate('/matchmaking');
+    }
+  };
 
   return (
     <div id="homecontainer">
       <img src={logo} alt="Logo" className="image" />
       <GoogleOAuthProvider clientId={client_id}>
         <GoogleLogin
-          onSuccess={credentialResponse => {
-            var decoded = jwtDecode(credentialResponse.credential);
+          onSuccess={(credentialResponse) => {
+            const decoded = jwtDecode(credentialResponse.credential);
             console.log(decoded);
-            navigate('/matchmaking');
+            responseGoogle(decoded);
           }}
           onError={() => {
             console.log('Login Failed');
--- src/firebaseservice.js
+++ src/firebaseservice.js
@@ -1,16 +1,29 @@
+// firebaseservice.js
+
 import { ref, set } from 'firebase/database';
 import { database } from './firebase';
 
-export const storeUserData = (userId, email, displayName) => {
+// Function to sanitize the email address for use in the database path
+const sanitizeEmail = (email) => {
+  return email.replace(/[.#$[\]/]/g, '_'); // Replace ".", "#", "$", "[", "]", and "/" with "_"
+};
+
+export const storeUserData = (email, displayName) => {
   try {
-    const userRef = ref(database, `users/${userId}`);
-    set(userRef, {
-      email,
-      displayName,
+    const sanitizedEmail = sanitizeEmail(email);
+    const userRef = ref(database, `users/${sanitizedEmail}`);
+    const userData = {
+      email: email,
+      displayName: displayName,
       wins: 0,
       winStreak: 0,
-    });
-    console.log('User data stored successfully:', userId);
+    };
+
+    console.log('Storing user data:', userData);
+
+    set(userRef, userData);
+
+    console.log('User data stored successfully:', displayName);
   } catch (error) {
     console.error('Error storing user data:', error);
   }
